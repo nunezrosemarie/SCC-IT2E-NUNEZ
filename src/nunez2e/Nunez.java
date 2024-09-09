75a8c3b4bfd2b3f1df42fef8d968bcd1253c3857
AshBalbz <ashbalbz04@gmail.com>
2024-09-09 17:25:00 +0800

    Updated Class

--- src/nunez2e/Nunez.java
+++ src/nunez2e/Nunez.java
@@ -4,9 +4,35 @@ package nunez2e;
 public class Nunez {
 
     public static void main(String[] args) {
-      grade grs = new grade();
+//      Grade grs = new Grade();
+//      grs.displayGrade();
       
-      grs.displayGrade();
+      Grades gr = new Grades();
+      gr.addGrades(100, "Rose", 2.0, 1.4, 1.0, 2.0);
+      gr.viewGrades();
+      
+//      Sale sls = new Sale();
+//      sls.getSale();
+//
+//      Sales sl = new Sales();
+//      sl.addSales("Marie", "Sabon", 5, 10, 30);
+//      sl.viewSales();
+//        
+//      Product pr = new Product();
+//      pr.getProduct();
+//      
+//      Products prs = new Products();
+//      prs.addProducts(101, "Sabon", 10, 20, 30);    
+//      prs.viewProducts();
+//
+//      Account ac = new Account();
+//      ac.displayAccounts();
+//      
+//      Accounts acs = new Accounts();
+//      acs.addAccounts(101, "Rosmar", "Nunez", "rosmar03@gmail.com", "Rosmar@123", "love123");
+//      acs.viewAccounts();
+
     }
-    
 }
+    
+
